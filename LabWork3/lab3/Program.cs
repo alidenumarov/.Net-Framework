c5d18607a85a444fe2aac414e2885a8288c3144f
Aliden Umarov <um_aliden_97@mail.ru>
2018-03-01 00:20:00 +0600

    Lab 3

---
+++ LabWork3/lab3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using library;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SquareArray squareArr = new SquareArray();
+            List<int> array = new List<int>();
+            List<int> squarredArray = new List<int>();
+            Console.Write("Enter array size: ");
+            int arrSize = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < arrSize; i++) {
+                Console.Write("Enter [" + (i + 1) + "] value: ");
+                int curArr = Convert.ToInt32(Console.ReadLine());
+                array.Add(curArr);
+            }
+            Console.Write("\n\n");
+            squarredArray = squareArr.ArrToSquare(array);
+            int cnt = 0;
+            foreach(var item in squarredArray) {
+                // Console.Write(item + " ");
+                cnt++;
+                Console.WriteLine("Squarred [" + cnt + "] value: " + item);
+                
+            }
+        }
+    }
+}
---
+++ LabWork3/library/SquareArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace library
+{
+    public class SquareArray {
+        public List<int> squaredArr;
+
+        public SquareArray()
+        {
+            
+        }
+        public List<int> ArrToSquare(List<int> arr) {
+            squaredArr = new List<int>();
+            for(int i = 0; i < arr.Count; i++) {
+                squaredArr.Add(arr[i] * arr[i]);
+            }
+            return squaredArr;
+        }
+    }
+}
